f79a29efa1a0ed9e8ff1d6ffd851020c6e744c4c
grigoscope <grigoriychepel1@gmail.com>
2026-08-11 21:17:42 +0300

    Создаль стандартную модель пользователя

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,20 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from .managers import UserManager
+
+class User(AbstractUser):
+    '''Стандартная модель пользователя.'''
+    username = None
+
+    email = models.EmailField(
+        verbose_name='Email-адрес', 
+        unique=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- config/settings.py
+++ config/settings.py
@@ -152,3 +152,5 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
+
+AUTH_USER_MODEL = 'accounts.User'
