67d8ac896d6b6c0a1c124ab2d8d8534ae584b426
grigoscope <grigoriychepel1@gmail.com>
2026-08-11 19:41:36 +0300

    Создал приложения по ТЗ

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', True)
+DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
